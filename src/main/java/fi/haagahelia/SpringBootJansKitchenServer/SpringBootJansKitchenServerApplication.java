4d3871a77b773a75edc491e685ae1e5cc6413ce0
htqtung <31605439+htqtung@users.noreply.github.com>
2018-05-04 21:45:45 +0300

    initial commit

---
+++ src/main/java/fi/haagahelia/SpringBootJansKitchenServer/SpringBootJansKitchenServerApplication.java
@@ -0,0 +1,12 @@
+package fi.haagahelia.SpringBootJansKitchenServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJansKitchenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJansKitchenServerApplication.class, args);
+	}
+}
